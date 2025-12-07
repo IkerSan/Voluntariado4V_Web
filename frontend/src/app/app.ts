84de6c6a0d6ad70d96b59343a94df1fe5fbcef1c
IkerSan <cosasquequieres@gmail.com>
2025-12-07 19:08:03 +0100

    Backend y frontend comunicados

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './services/api.service';
 
 
 @Component({
@@ -8,7 +9,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Voluntariado 4V');
-  
+  protected usuarios = signal<any[]>([]);
+  private apiService = inject(ApiService);
+
+  ngOnInit() {
+    this.apiService.getUsuarios().subscribe({
+      next: (data) => {
+        console.log('Datos recibidos del backend:', data);
+        this.usuarios.set(data);
+      },
+      error: (err) => console.error('Error al conectar con el backend:', err)
+    });
+  }
 }
---
+++ frontend/src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8000/api'; // Ajusta esto si tu backend corre en otro puerto
+
+  constructor() { }
+
+  getUsuarios(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/usuarios`);
+  }
+}
